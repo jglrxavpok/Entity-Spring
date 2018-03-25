fc46ad36b22e06527cfdd8cf510cbc6c48df780c
jglrxavpok <jglrxavpok@gmail.com>
2018-03-25 20:19:28 +0200

    Forgot to change version number in Mod class

--- src/main/java/org/jglrxavpok/spring/EntitySpringMod.java
+++ src/main/java/org/jglrxavpok/spring/EntitySpringMod.java
@@ -12,7 +12,7 @@ public class EntitySpringMod
 {
     public static final String MODID = "entityspring";
     public static final String NAME = "Entity Spring";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
     @Mod.Instance(MODID)
     public static EntitySpringMod instance;
     public SpringItem springItemInstance = new SpringItem();
