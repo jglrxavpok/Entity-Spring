e3d3d4ddc0d8b114ad5ba7b3e6ebb1209ed3f30c
bl4ckscor3 <bl4ckscor33@gmail.com>
2019-12-14 12:41:54 +0100

    fix mod not starting on dedicated server

--- src/main/java/org/jglrxavpok/spring/client/ClientEventSink.java
+++ src/main/java/org/jglrxavpok/spring/client/ClientEventSink.java
@@ -1,8 +1,6 @@
 package org.jglrxavpok.spring.client;
 
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
@@ -10,10 +8,10 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import org.jglrxavpok.spring.EntitySpringMod;
 import org.jglrxavpok.spring.common.SpringEntity;
 
-@OnlyIn(Dist.CLIENT)
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = EntitySpringMod.MODID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = EntitySpringMod.MODID, value = Dist.CLIENT)
 public class ClientEventSink {
 
+    @SubscribeEvent
     public static void preInit(FMLClientSetupEvent event) {
         RenderingRegistry.registerEntityRenderingHandler(SpringEntity.class, RenderSpringEntity::new);
     }
